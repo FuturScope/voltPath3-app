090d53b684b354752632ce3abe15ddfa0451d7a6
Leticia Attoh <leticiaattoh1994@gmail.com>
2024-11-06 15:56:53 +0000

    fixed inbox route

--- src/components/inbox.jsx
+++ src/components/inbox.jsx
@@ -6,16 +6,13 @@ const Inbox = () => {
   return (
     <div>
       <div>
-        <SearchBar/>
+        <SearchBar />
       </div>
       <div className="absolute top-1/4 w-[70%] text-center text-white px-4 mt-[10%] md:px-0 z-10">
         <h1 className="text-3xl font-bold md:text-4xl lg:text-3xl mb-4 opacity-90 flex items-center justify-center">
           <FaInbox className="mr-2 text-yellow-400" /> {/* Use the car icon */}
           No messages received yet
         </h1>
-      
-          
-       
       </div>
     </div>
   );
