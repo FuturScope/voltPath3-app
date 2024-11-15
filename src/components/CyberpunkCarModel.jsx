62f510ca10542c0658c4981d8de3ae8217738c86
Leticia Attoh <leticiaattoh1994@gmail.com>
2024-11-15 09:11:45 +0000

    removed first from overlay

--- src/components/CyberpunkCarModel.jsx
+++ src/components/CyberpunkCarModel.jsx
@@ -25,7 +25,7 @@ const CyberpunkCarModel = () => {
     >
       <div className="absolute top-1/4 w-full text-center text-white px-4 md:px-0 z-10">
         <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl mb-4 opacity-90">
-          Welcome to Ghana's First EV Charging Hub
+          Welcome to Ghana's EV Charging Hub
         </h1>
         <p className="text-lg md:text-xl lg:text-2xl font-medium max-w-2xl mx-auto opacity-75">
           Providing comprehensive solutions for all your electric vehicle
